1915894dec116a126c8e9591ebbd6616c0ebc1f5
wupeng8779 <877924801@qq.com>
2020-11-25 16:46:47 +0800

    2

--- src/TestTag.java
+++ src/TestTag.java
@@ -3,5 +3,6 @@ public class TestTag {
     public static void main(String[] args) {
         System.out.println("init");
         System.out.println("1");
+        System.out.println("2");
     }
 }
